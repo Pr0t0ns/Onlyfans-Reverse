46039b7d80d993a16bd74f1b8333748be9e8fc9d
Pr0t0n <105520163+Pr0t0ns@users.noreply.github.com>
2024-11-24 21:27:51 -0600

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,6 +1,6 @@
 #### NOTES #### 
 # - You will need to replace all the OF Operation functions any update that's pushed that contains a modified logic
-# - Operations in functions should stay relavent during updates but the sign key, First Vector of Sign and sign version must be updated for sign to be validated
+# - Operations in functions should stay relavent during updates but the sign key, First Vector of Sign and final sector of sign must be updated for sign to be validated
 # - https://static2.onlyfans.com/static/prod/f/202411221209-ff9b8e2d99/2313.js <-- File where sign is produced (If link isn't valid replace "202411221209-ff9b8e2d99" with the current version)
 # - n.A = W =>  ||| <-- function where sign is produced
 #### ##### #####
