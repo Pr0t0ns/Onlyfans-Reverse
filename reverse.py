33a6176fab998449b01c3d07d8741a46415f4a76
Pr0t0n <105520163+Pr0t0ns@users.noreply.github.com>
2024-11-26 01:07:19 -0600

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -4,11 +4,8 @@ import base64
 import time
 from mod import initalize_onlyfans_configuration
 
-
-
-
 class OnlyFans:
-    def __init__(self, user_agent: str, verison: str="202411221209-ff9b8e2d99") -> None:
+    def __init__(self, user_agent: str, verison: str="202411250920-9b8114829c") -> None:
         self.ua = user_agent
         self.v = verison
         self.st = int(time.time()) * 1000
@@ -39,42 +36,39 @@ class OnlyFans:
             pass
         data = f"{self.secret}\n{self.st}\n{endpoint}\n{params}"
         W = hashlib.sha1(data.encode()).hexdigest()
-        t = {
-            "time": self.st
-        }
         res = hex(abs(
-            ord(W[35360 % len(W)]) - 103 +
-            (ord(W[34780 % len(W)]) + 108) +
-            (ord(W[35751 % len(W)]) + 63) +
-            (ord(W[34550 % len(W)]) - 79) +
-            (ord(W[34700 % len(W)]) - 63) +
-            (ord(W[34323 % len(W)]) - 136) +
-            (ord(W[35073 % len(W)]) - 122) +
-            (ord(W[33530 % len(W)]) + 130) +
-            (ord(W[33426 % len(W)]) - 107) +
-            (ord(W[33582 % len(W)]) + 66) +
-            (ord(W[35257 % len(W)]) + 107) +
-            (ord(W[34833 % len(W)]) - 70) +
-            (ord(W[33337 % len(W)]) + 113) +
-            (ord(W[33766 % len(W)]) + 84) +
-            (ord(W[33249 % len(W)]) + 55) +
-            (ord(W[35175 % len(W)]) + 109) +
-            (ord(W[34206 % len(W)]) - 145) +
-            (ord(W[35620 % len(W)]) - 105) +
-            (ord(W[34952 % len(W)]) - 127) +
-            (ord(W[33974 % len(W)]) + 122) +
-            (ord(W[34426 % len(W)]) - 129) +
-            (ord(W[35695 % len(W)]) - 90) +
-            (ord(W[35519 % len(W)]) - 84) +
-            (ord(W[33138 % len(W)]) + 100) +
-            (ord(W[34493 % len(W)]) - 96) +
-            (ord(W[33056 % len(W)]) - 49) +
-            (ord(W[34091 % len(W)]) - 128) +
-            (ord(W[35466 % len(W)]) + 116) +
-            (ord(W[33874 % len(W)]) + 126) +
-            (ord(W[34649 % len(W)]) - 124) +
-            (ord(W[33195 % len(W)]) - 68) +
-            (ord(W[33688 % len(W)]) + 130)
+            ord(W[35975 % len(W)]) + 129 +
+            (ord(W[34586 % len(W)]) - 135) +
+            (ord(W[35853 % len(W)]) + 79) +
+            (ord(W[34776 % len(W)]) - 109) +
+            (ord(W[33882 % len(W)]) + 110) +
+            (ord(W[33534 % len(W)]) - 131) +
+            (ord(W[34477 % len(W)]) - 96) +
+            (ord(W[35561 % len(W)]) - 86) +
+            (ord(W[35484 % len(W)]) + 82) +
+            (ord(W[35787 % len(W)]) + 99) +
+            (ord(W[35612 % len(W)]) - 79) +
+            (ord(W[34897 % len(W)]) - 138) +
+            (ord(W[33433 % len(W)]) + 97) +
+            (ord(W[35699 % len(W)]) - 106) +
+            (ord(W[33939 % len(W)]) - 76) +
+            (ord(W[33606 % len(W)]) + 78) +
+            (ord(W[35341 % len(W)]) - 126) +
+            (ord(W[35145 % len(W)]) - 110) +
+            (ord(W[34055 % len(W)]) + 123) +
+            (ord(W[34124 % len(W)]) - 81) +
+            (ord(W[34691 % len(W)]) - 108) +
+            (ord(W[33361 % len(W)]) - 124) +
+            (ord(W[33254 % len(W)]) + 114) +
+            (ord(W[35044 % len(W)]) + 102) +
+            (ord(W[33798 % len(W)]) - 127) +
+            (ord(W[35430 % len(W)]) - 95) +
+            (ord(W[34946 % len(W)]) - 51) +
+            (ord(W[34201 % len(W)]) - 102) +
+            (ord(W[34394 % len(W)]) + 134) +
+            (ord(W[33677 % len(W)]) - 84) +
+            (ord(W[35228 % len(W)]) - 111) +
+            (ord(W[34286 % len(W)]) - 99)
         ))[2:]
 
         return {"sign": f"{self.first_sign_segment}:{W}:{res}:{self.final_sign_segment}", "Time": self.st, "x-bc": self.x_bc(), "x-of-rev": self.v, "user-agent": self.ua, "endpoint": fe, "computing_time": round(time.time()-t1, 6)}
