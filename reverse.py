aa3b5f6abaa5f806a80a3c0cf0fbdbed90ba7e26
Pr0t0n <105520163+Pr0t0ns@users.noreply.github.com>
2025-01-22 20:04:51 -0600

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -2,17 +2,32 @@ import random
 import hashlib
 import base64
 import time
-from mod import initalize_onlyfans_configuration
+import tls_client
+from mod import MOD
 
 class OnlyFans:
-    def __init__(self, user_agent: str, verison: str="202411250920-9b8114829c") -> None:
+    def __init__(self, user_agent: str, secrets: list=None) -> None:
         self.ua = user_agent
-        self.v = verison
         self.st = int(time.time()) * 1000
-        self.n = initalize_onlyfans_configuration()
-        self.secret = self.n['reRtQ']
-        self.first_sign_segment = self.n['QvSwh']
-        self.final_sign_segment = self.n['HrBVl']
+        if secrets == None or len(secrets) != 2:
+            self.n, self.v = MOD.initalize_onlyfans_configuration()
+            self.secrets = [self.n['sign_key'], self.n['first_segment'], self.n['final_segment']]
+        else:
+            self.v = MOD.fetch_X_OF_REV()
+            self.secrets = secrets
+
+    @staticmethod
+    def fetch_X_HASH(proxy: str=None):
+        session = tls_client.Session(client_identifier="chrome_131", random_tls_extension_order=True)
+        if proxy != None:
+            session.proxies = {
+                'https': f'http://{proxy}',
+                'http': f'http://{proxy}'
+            }
+        try:
+            return session.get("https://cdn2.onlyfans.com/hash/").text
+        except:
+            return None
 
     def x_bc(self) -> str:
         _list= [self.st, OnlyFans.he(), OnlyFans.he(), self.ua]
@@ -34,47 +49,52 @@ class OnlyFans:
             endpoint = endpoint.split("://onlyfans.com")[1]
         except:
             pass
-        data = f"{self.secret}\n{self.st}\n{endpoint}\n{params}"
+        data = f"{self.secrets[0]}\n{self.st}\n{endpoint}\n{params}"
         W = hashlib.sha1(data.encode()).hexdigest()
-        res = hex(abs(
-            ord(W[35975 % len(W)]) + 129 +
-            (ord(W[34586 % len(W)]) - 135) +
-            (ord(W[35853 % len(W)]) + 79) +
-            (ord(W[34776 % len(W)]) - 109) +
-            (ord(W[33882 % len(W)]) + 110) +
-            (ord(W[33534 % len(W)]) - 131) +
-            (ord(W[34477 % len(W)]) - 96) +
-            (ord(W[35561 % len(W)]) - 86) +
-            (ord(W[35484 % len(W)]) + 82) +
-            (ord(W[35787 % len(W)]) + 99) +
-            (ord(W[35612 % len(W)]) - 79) +
-            (ord(W[34897 % len(W)]) - 138) +
-            (ord(W[33433 % len(W)]) + 97) +
-            (ord(W[35699 % len(W)]) - 106) +
-            (ord(W[33939 % len(W)]) - 76) +
-            (ord(W[33606 % len(W)]) + 78) +
-            (ord(W[35341 % len(W)]) - 126) +
-            (ord(W[35145 % len(W)]) - 110) +
-            (ord(W[34055 % len(W)]) + 123) +
-            (ord(W[34124 % len(W)]) - 81) +
-            (ord(W[34691 % len(W)]) - 108) +
-            (ord(W[33361 % len(W)]) - 124) +
-            (ord(W[33254 % len(W)]) + 114) +
-            (ord(W[35044 % len(W)]) + 102) +
-            (ord(W[33798 % len(W)]) - 127) +
-            (ord(W[35430 % len(W)]) - 95) +
-            (ord(W[34946 % len(W)]) - 51) +
-            (ord(W[34201 % len(W)]) - 102) +
-            (ord(W[34394 % len(W)]) + 134) +
-            (ord(W[33677 % len(W)]) - 84) +
-            (ord(W[35228 % len(W)]) - 111) +
-            (ord(W[34286 % len(W)]) - 99)
+        res = hex((
+            abs(
+                ord(W[33848 % len(W)]) - 89 +
+                (ord(W[34604 % len(W)]) - 119) +
+                (ord(W[34328 % len(W)]) - 93) +
+                (ord(W[34828 % len(W)]) + 100) +
+                (ord(W[34740 % len(W)]) + 108) +
+                (ord(W[34497 % len(W)]) - 120) +
+                (ord(W[34011 % len(W)]) - 112) +
+                (ord(W[34984 % len(W)]) + 110) +
+                (ord(W[33584 % len(W)]) + 104) +
+                (ord(W[35840 % len(W)]) + 76) +
+                (ord(W[34174 % len(W)]) - 105) +
+                (ord(W[35501 % len(W)]) - 128) +
+                (ord(W[35050 % len(W)]) + 76) +
+                (ord(W[35386 % len(W)]) + 76) +
+                (ord(W[35764 % len(W)]) - 101) +
+                (ord(W[34652 % len(W)]) + 76) +
+                (ord(W[35888 % len(W)]) + 64) +
+                (ord(W[35570 % len(W)]) - 87) +
+                (ord(W[34378 % len(W)]) + 60) +
+                (ord(W[35167 % len(W)]) + 79) +
+                (ord(W[33783 % len(W)]) + 107) +
+                (ord(W[33699 % len(W)]) - 118) +
+                (ord(W[33409 % len(W)]) + 111) +
+                (ord(W[35119 % len(W)]) - 84) +
+                (ord(W[35683 % len(W)]) - 116) +
+                (ord(W[34882 % len(W)]) + 56) +
+                (ord(W[33926 % len(W)]) + 84) +
+                (ord(W[35336 % len(W)]) - 125) +
+                (ord(W[34259 % len(W)]) - 104) +
+                (ord(W[33496 % len(W)]) - 111) +
+                (ord(W[34099 % len(W)]) + 107) +
+                (ord(W[35219 % len(W)]) + 71)
+            )
         ))[2:]
 
-        return {"sign": f"{self.first_sign_segment}:{W}:{res}:{self.final_sign_segment}", "Time": self.st, "x-bc": self.x_bc(), "x-of-rev": self.v, "user-agent": self.ua, "endpoint": fe, "computing_time": round(time.time()-t1, 6)}
+        return {"sign": f"{self.secrets[1]}:{W}:{res}:{self.secrets[2]}", "Time": self.st, "x-bc": self.x_bc(), "x-of-rev": self.v, "user-agent": self.ua, "endpoint": fe, "computing_time": round(time.time()-t1, 6)}
  
+
+
 
 if __name__ == "__main__":
     OF = OnlyFans(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
     computed_signature = (OF.generate_header_data("https://onlyfans.com/api2/v2/users/me"))
-    print(computed_signature)
+    X_hash = OF.fetch_X_HASH()
+    print(computed_signature, X_hash)
